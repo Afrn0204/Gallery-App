1e80f5770b98247e2751a6586c9fd1c209dd98ea
Afrn0204 <afrianvw@gmail.com>
2024-10-29 21:22:48 +0700

    new upload files

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Afrian Viky Widyantono"; // TODO: replace this
+const studentId = "FS12052950"; // TODO: replace this
+
+export { url, studentName, studentId };
